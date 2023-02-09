2318014a1c17657e8160ea999e18cd8e77c8f420
GabrielMr897 <gabriel-pms@hotmail.com>
2023-02-09 20:30:44 -0300

    first commit

---
+++ src/test/java/br/com/apiEM/ApiEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apiEM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
